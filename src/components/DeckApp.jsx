51783c251e5ba7fa9a6832716e393882a434e817
Safali <safalib12@gmail.com>
2025-10-28 21:23:52 -0400

    Initialize 52-card deck with suits, values and drawnCards state

--- src/components/DeckApp.jsx
+++ src/components/DeckApp.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
-
+import Card from './card';
+import '../styles/deck.css';
 
 
 function DeckApp() {
+  const suits = ['♠', '♥', '♦', '♣'];
+  const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  //Initialize deck of 52 cards
+  const initializeDeck = () => {
+    let id = 0;
+    const deck = [];
+    suits.forEach((suit) => {
+      values.forEach((value) => {
+        deck.push({ id: id++, suit, value });
+      });
+    });
+    return deck;
+  };
+  const [deck, setDeck] = useState(initializeDeck());
+  const [drwanCards, setDrawnCards] = useState([]);
+
+  //Click handler for deck rectangle(future logic)
+  const handleDeckClick = () => {
+    console.log('Deck clicked! Cards remaining:', deck.length);
+  };
   return (
     <div>
+      <div className="deck-rectangle mx-auto my-4"
+        onClick={handleDeckClick}>
+          {deck.length ===0 && <span className="text-white">No cards remaining</span>} 
+      </div>
+
+      
       <div className="deck-rectangle mx-auto my-4"></div>
       <div>
         <button className="btn btn-primary mx-1">Deal 5</button>
