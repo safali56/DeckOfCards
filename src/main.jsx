cab0ae946a677f0afdb347beb744887bdce4f877
Safali <safalib12@gmail.com>
2025-10-28 23:40:55 -0400

    Addig SOA

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+/**StAuth10244: I Safali BC Senchuri, 000931218, certify that this material is my original work. 
+ * No other person's work has been used without due acknowledgement. 
+ * I have not made my work available to anyone else." */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
